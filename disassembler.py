517bc066ad5d7eb5dbed11a5494a2ad6656f29d2
Benedikt Waibel <49001513+Alkalem@users.noreply.github.com>
2025-06-03 14:51:22 +0200

    Implemented decoding of fixed fields, matching an encoded instruction to opcodes.
    Multiple functional units can match an instruction because the formats are not fully exclusive.
    For one format (likely always the most constrained one), opcodes will match the instruction.
    There can be multiple candidates because the list includes opcodes that purposefully overlap.
    For example, an assembler macro ZERO REG realised as MVK 0, REG.

--- disassembler.py
+++ disassembler.py
@@ -1,10 +1,11 @@
 from .constants import WORD_SIZE, Endianness, Register
 
 from dataclasses import dataclass
-from typing import Optional, List
+from typing import Optional, List, Dict
 from pathlib import Path
 import json
 from types import SimpleNamespace as Namespace
+
 
 @dataclass
 class Instruction:
@@ -18,7 +19,7 @@ class Instruction:
 
 @dataclass
 class Field:
-    name:str
+    id:str
     pos:int
     width:int
 
@@ -29,10 +30,38 @@ class InstructionFormat:
     mask:int
     fields:List[Field]
 
+@dataclass
+class FixedField:
+    id:str
+    min:int
+    max:int
+
+@dataclass
+class VarField:
+    id:str
+    method:str
+    op:int
+
+@dataclass
+class Opcode:
+    name:str
+    unit:str
+    format:str
+    type:str
+    isa:int
+    flags:int
+    fixed:List[FixedField]
+    ops:List[str]
+    vars:List[VarField]
+
+
 def format_decoder(obj:dict):
     if {'name', 'key', 'mask', 'fields'}.issubset(obj.keys()):
         return InstructionFormat(obj['name'], int(obj['key'], 0),
                 int(obj['mask'], 0), obj['fields'])
+    elif {'name', 'pos', 'width'}.issubset(obj.keys()):
+        return Field(obj['name'], obj['pos'], obj['width'])
+
 
 class Disassembler:
     def __init__(self, endian:Endianness=Endianness.LITTLE) -> None:
@@ -42,6 +71,18 @@ class Disassembler:
         with open(basepath / 'instruction_formats.json') as file:
             self.instruction_formats:List[InstructionFormat] = json.load(
                 file, object_hook=format_decoder)
+            self.instruction_formats.sort(
+                key=lambda f: -f.mask.bit_count())
+            self.instruction_maps:Dict[str, List[Opcode]] = {
+                f.name: list()
+                for f in self.instruction_formats}
+        with open(basepath / 'opcodes.json') as file:
+            opcodes:List[Opcode] = json.load(
+                file, object_hook=lambda obj: Namespace(**obj))
+            for opcode in opcodes:
+                format = opcode.unit+'_'+opcode.format
+                if format in self.instruction_maps:
+                    self.instruction_maps[format].append(opcode)
 
     def disasm(self, data:bytes, address:int, count:int=-1):
         remaining = data
@@ -62,7 +103,29 @@ class Disassembler:
             count -= 1
 
     def __decode(self, encoded:int) -> str:
+        instr = "?"
         for format in self.instruction_formats:
             if encoded & format.mask == format.key:
-                return format.name
+                print('unit', format.name)
+                fields = {
+                    field.id: self.__decode_field(field, encoded)
+                    for field in format.fields
+                }
+
+                for opcode in self.instruction_maps[format.name]:
+                    if all({ self.__matches_fixed(fields, fixed)
+                            for fixed in opcode.fixed}):
+                        print(opcode.name, opcode)
+        if instr: return instr
         raise ValueError()
+    
+    def __decode_field(self, field:Field, encoded:int) -> int:
+        mask = (1<<field.width) - 1
+        return (encoded>>field.pos) & mask
+    
+    def __matches_fixed(self, fields:Dict[str, int], 
+            fixed:FixedField) -> bool:
+        if fixed.id not in fields: 
+            raise ValueError()
+        return fixed.min <= fields[fixed.id] <= fixed.max
+
