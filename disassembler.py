49e7ec97666fb8d786d392f252c0d7ea308be5b4
Benedikt Waibel <49001513+Alkalem@users.noreply.github.com>
2026-01-24 16:53:55 +0100

    Set GPLv3 license for project and added license notices.

--- disassembler.py
+++ disassembler.py
@@ -1,3 +1,19 @@
+# Copyright 2025-2026 Benedikt Waibel
+# 
+# This file is part of the library tms320c6x_disassembler.
+# 
+# The tms320c6x_disassembler is free software: 
+# you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
 from .constants import FETCH_PACKET_SIZE, WORD_SIZE, C64XP, \
         EXECUTION_PACKET_LIMIT, HEADER_MASK, HEADER_KEY, HEADER_PBITS_MASK, \
         HEADER_FIELD_MASK, HEADER_LAYOUT_OFFSET, HEADER_EXPANSION_OFFSET, \
